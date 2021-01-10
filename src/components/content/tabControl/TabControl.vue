874ad25f935bf26e4cb9525b9402efa086f2e769
YG-07 <1030138554@qq.com>
2021-01-10 15:22:37 +0800

    update

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="tab-control">
+    <div class="tab-control-item" :class="{active: index === currentIndex}"
+      v-for="(item, index) in titles" @click="itemClick(index)" :key="index">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  //只是文字不一样时，可以不用slot，通过组件props传递数据
+
+  export default {
+    name: "TabControl",
+    props: {
+      titles: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    },
+    data() {
+      return {
+        currentIndex: 0
+      }
+    },
+    methods: {
+      itemClick(index) {
+        this.currentIndex = index
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-control {
+    display: flex;
+    text-align: center;
+    font-size: 15px;
+    height: 40px;
+    line-height: 40px;
+    background-color: #fff;
+  }
+  .tab-control-item {
+    flex: 1;
+  }
+  .tab-control-item span {
+    padding: 5px;
+  }
+  .active {
+    color: var(--color-high-text);
+  }
+  .active span {
+    border-bottom: 3px solid var(--color-tint);
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,16 +3,74 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
-    <home-swiper :banners="banners"/>
-    <home-recommend :recommends="recommends"/>
+    <home-swiper :banners="banners" />
+    <home-recommend :recommends="recommends" />
+    <home-feature />
+    <tab-control class="tab-control" :titles="titles" />
+    <ul>
+      <li>文字1</li>
+      <li>文字2</li>
+      <li>文字3</li>
+      <li>文字4</li>
+      <li>文字5</li>
+      <li>文字6</li>
+      <li>文字7</li>
+      <li>文字8</li>
+      <li>文字9</li>
+      <li>文字10</li>
+      <li>文字11</li>
+      <li>文字12</li>
+      <li>文字13</li>
+      <li>文字14</li>
+      <li>文字15</li>
+      <li>文字16</li>
+      <li>文字17</li>
+      <li>文字18</li>
+      <li>文字19</li>
+      <li>文字20</li>
+      <li>文字21</li>
+      <li>文字22</li>
+      <li>文字23</li>
+      <li>文字24</li>
+      <li>文字25</li>
+      <li>文字26</li>
+      <li>文字27</li>
+      <li>文字28</li>
+      <li>文字29</li>
+      <li>文字30</li>
+      <li>文字31</li>
+      <li>文字32</li>
+      <li>文字33</li>
+      <li>文字34</li>
+      <li>文字35</li>
+      <li>文字36</li>
+      <li>文字37</li>
+      <li>文字38</li>
+      <li>文字39</li>
+      <li>文字40</li>
+      <li>文字41</li>
+      <li>文字42</li>
+      <li>文字43</li>
+      <li>文字44</li>
+      <li>文字45</li>
+      <li>文字46</li>
+      <li>文字47</li>
+      <li>文字48</li>
+      <li>文字49</li>
+      <li>文字50</li>
+    </ul>
   </div>
 </template>
 
 <script>
-  import NavBar from 'components/common/navbar/NavBar'
+  // 界面子组件
   import HomeSwiper from './childComps/HomeSwiper'
   import HomeRecommend from './childComps/HomeRecommend'
-
+  import HomeFeature from './childComps/HomeFeature'
+  // 公共组件
+  import NavBar from 'components/common/navbar/NavBar'
+  import TabControl from 'components/content/tabControl/TabControl'
+  // 网络请求js
   import {getHomeMultidata} from 'network/home'
 
   export default {
@@ -20,13 +78,16 @@
     data() {
       return {
         banners: [],
-        recommends: []
+        recommends: [],
+        titles: ['流行','新款','精选']
       }
     },
     components: {
-      NavBar,
       HomeSwiper,
-      HomeRecommend
+      HomeRecommend,
+      HomeFeature,
+      NavBar,
+      TabControl
     },
     created() {
       getHomeMultidata().then(res => {
@@ -39,8 +100,23 @@
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+    /* 底部间隙 待优化 */
+    padding-bottom: 100px;
+  }
   .home-nav {
     color: #fff;
-    background-color: var(--color-tint)
+    background-color: var(--color-tint);
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+  .tab-control {
+    /* sticky达到position的值时就变成fixed */
+    position: sticky;
+    top: 44px;
   }
 </style>
